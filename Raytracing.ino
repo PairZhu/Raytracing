36d96fde63364850f899a999b146b6dd39f7c249
PairZhu <47098840+PairZhu@users.noreply.github.com>
2022-12-07 23:07:56 +0800

    [feat]修改指令返回格式

--- Raytracing.ino
+++ Raytracing.ino
@@ -4,6 +4,9 @@
 #include "LightSensor.h"
 #include "PID.h"
 
+// 宏函数，用于将布尔值转换为字符串
+#define BOOL2STR(x) ((x) ? "true" : "false")
+
 constexpr int flagAddr = 0;
 constexpr int paramsAddr = 1;
 
@@ -21,15 +24,15 @@ int angleY = 90;
 LightSensor lightSensorUp(A0, 1, 0);
 LightSensor lightSensorDown(A1, 1.19, 0);
 LightSensor lightSensorLeft(A2, 1, 0);
-LightSensor lightSensorRight(A3, 2.31, -700);
+LightSensor lightSensorRight(A3, 2.31, -500);
 // 定义PID结构体变量
 PID pidX(0.010000, 0.000200, 0.010000, 0, 5, 250, 5);
 PID pidY(0.030000, 0.000500, 0.050000, 0, 5, 250, 5);
 
 void setup() {
     // 初始化串口
-    Serial.begin(9600);
-    Serial.setTimeout(10);
+    Serial.begin(115200);
+    Serial.setTimeout(2);
     // 初始化两个舵机，脉冲宽度范围为500-2500us
     servoX.attach(9, 500, 2500);
     servoY.attach(10, 500, 2500);
@@ -43,11 +46,11 @@ void setup() {
     lightSensorRight.init();
     if (EEPROM.read(flagAddr) == 'T') {
         // 如果EEPROM中有数据则从中读取参数
-        Serial.println("Read parameters from EEPROM");
+        Serial.println("{\"msg\":\"Read parameters from EEPROM\"}");
         readParams();
     } else {
         // 否则将参数写入EEPROM，并将标志设置为'T'
-        Serial.println("Write parameters to EEPROM");
+        Serial.println("{\"msg\":\"Write parameters to EEPROM\"}");
         writeParams();
         EEPROM.write(flagAddr, 'T');
     }
@@ -93,10 +96,11 @@ void raytracing() {
         if (++cnt >= 50) {
             cnt = 0;
             char str[30];
-            sprintf(str, "errorX: %d errorY: %d\n", (int)(errorX),
+            sprintf(str, "{\"errorX\":%d,\"errorY\":%d,", (int)(errorX),
                     (int)(errorY));
             Serial.print(str);
-            sprintf(str, "Up: %ld Down: %ld\nLeft: %ld Right: %ld\n",
+            sprintf(str,
+                    "\"Up\":%ld,\"Down\":%ld,\"Left\":%ld,\"Right\":%ld}\n",
                     (long)lightUp, (long)lightDown, (long)lightLeft,
                     (long)lightRight);
             Serial.print(str);
@@ -111,60 +115,78 @@ void readCmd() {
     }
     char cmd = Serial.read();
     switch (cmd) {
+        case '-':  // Write X Angle
+            angleX = Serial.parseInt();
+            servoX.write(angleX);
+            Serial.println("{\"angleX\":" + String(angleX) + "}");
+            break;
+        case '|':  // Write Y Angle
+            angleY = Serial.parseInt();
+            servoY.write(angleY);
+            Serial.println("{\"angleY\":" + String(angleY) + "}");
+            break;
+        case '#':  // Read State
+            Serial.println("{\"angleX\":" + String(angleX) +
+                           ",\"angleY\":" + String(angleY) +
+                           ",\"Horizontal\":" + BOOL2STR(horizontalControl) +
+                           ",\"Vertical\":" + BOOL2STR(verticalControl) +
+                           ",\"Debug\":" + BOOL2STR(debug) + "}");
+            break;
         case 'H':  // Horizontal
             horizontalControl = !horizontalControl;
-            Serial.println("Horizontal: " +
-                           String(horizontalControl ? "on" : "off"));
+            Serial.println(String("{\"Horizontal\":") + BOOL2STR(horizontalControl) +
+                           "}");
             break;
         case 'V':  // Vertical
             verticalControl = !verticalControl;
-            Serial.println("Vertical: " +
-                           String(verticalControl ? "on" : "off"));
+            Serial.println(String("{\"Vertical\":") + BOOL2STR(verticalControl) + "}");
             break;
         case '!':  // Debug
             debug = !debug;
-            Serial.println("Debug: " + String(debug ? "on" : "off"));
+            Serial.println(String("{\"Debug\":") + BOOL2STR(debug) + "}");
             break;
         case 'D':  // Down
             lightSensorDown.k = Serial.parseFloat();
             lightSensorDown.b = Serial.parseFloat();
-            Serial.println("Down: k=" + String(lightSensorDown.k) +
-                           ", b=" + String(lightSensorDown.b));
+            Serial.println("{\"k\":" + String(lightSensorDown.k) +
+                           ",\"b\":" + String(lightSensorDown.b) + "}");
             break;
         case 'R':  // Rright
             lightSensorRight.k = Serial.parseFloat();
             lightSensorRight.b = Serial.parseFloat();
-            Serial.println("Right: k=" + String(lightSensorRight.k) +
-                           ", b=" + String(lightSensorRight.b));
+            Serial.println("{\"k\":" + String(lightSensorRight.k) +
+                           ",\"b\":" + String(lightSensorRight.b) + "}");
             break;
         case 'X':  // pidX
             pidX.kp = Serial.parseFloat();
             pidX.ki = Serial.parseFloat();
             pidX.kd = Serial.parseFloat();
-            Serial.println("pidX: kp=" + String(pidX.kp, 6) + ", ki=" +
-                           String(pidX.ki, 6) + ", kd=" + String(pidX.kd, 6));
+            Serial.println("{\"kp\":" + String(pidX.kp, 6) +
+                           ",\"ki\":" + String(pidX.ki, 6) +
+                           ",\"kd\":" + String(pidX.kd, 6) + "}");
             break;
         case 'Y':  // pidX
             pidY.kp = Serial.parseFloat();
             pidY.ki = Serial.parseFloat();
             pidY.kd = Serial.parseFloat();
-            Serial.println("pidY: kp=" + String(pidY.kp, 6) + ", ki=" +
-                           String(pidY.ki, 6) + ", kd=" + String(pidY.kd, 6));
+            Serial.println("{\"kp\":" + String(pidY.kp, 6) +
+                           ",\"ki\":" + String(pidY.ki, 6) +
+                           ",\"kd\":" + String(pidY.kd, 6) + "}");
             break;
         case 'T':  // Threshold
             lightThreshold = Serial.parseInt();
-            Serial.println("Threshold: " + String(lightThreshold));
+            Serial.println("{\"Threshold\":" + String(lightThreshold) + "}");
             break;
         case 'P':  // Print
             printParams();
             break;
         case 'S':  // Save
             writeParams();
-            Serial.println("Saved");
+            Serial.println("{\"msg\":\"Saved\"}");
             break;
         case 'C':  // Clear
             EEPROM.write(flagAddr, 0);
-            Serial.println("Cleared, restarting...\n\n");
+            Serial.println("{\"msg\":\"Cleared, restarting...\"}");
             delay(1000);
             // 调用0地址的函数，重启
             ((void (*)())0)();
@@ -224,15 +246,15 @@ void writeParams() {
 
 void printParams() {
     // 输出当前光照传感器的参数
-    Serial.println("Down: k=" + String(lightSensorDown.k) +
-                   ", b=" + String(lightSensorDown.b));
-    Serial.println("Right: k=" + String(lightSensorRight.k) +
-                   ", b=" + String(lightSensorRight.b));
+    Serial.print("{\"Down\":{\"k\":" + String(lightSensorDown.k) +
+                 ",\"b\":" + String(lightSensorDown.b) + "},");
+    Serial.print("\"Right\":{\"k\":" + String(lightSensorRight.k) +
+                 ",\"b\":" + String(lightSensorRight.b) + "},");
     // 输出当前PID的参数
-    Serial.println("pidX: kp=" + String(pidX.kp, 6) +
-                   ", ki=" + String(pidX.ki, 6) + ", kd=" + String(pidX.kd, 6));
-    Serial.println("pidY: kp=" + String(pidY.kp, 6) +
-                   ", ki=" + String(pidY.ki, 6) + ", kd=" + String(pidY.kd, 6));
+    Serial.print("\"pidX\":{\"kp\":" + String(pidX.kp, 6) + ",\"ki\":" +
+                 String(pidX.ki, 6) + ",\"kd\":" + String(pidX.kd, 6) + "},");
+    Serial.print("\"pidY\":{\"kp\":" + String(pidY.kp, 6) + ",\"ki\":" +
+                 String(pidY.ki, 6) + ",\"kd\":" + String(pidY.kd, 6) + "},");
     // 输出当前阈值
-    Serial.println("Threshold: " + String(lightThreshold));
+    Serial.println("\"Threshold\":" + String(lightThreshold) + "}");
 }
